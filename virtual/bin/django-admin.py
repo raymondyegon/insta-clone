5c98a77a7644d51ceffd8a94aaa668f4b3a47026
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-17 11:36:54 +0300

    virtual environment set

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ray/Documents/moringa/python/django/instagram/virtual/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
