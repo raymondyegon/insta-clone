d9391114d183c75689d1d97123a169ff628be671
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-19 16:11:03 +0300

    django-registration added

--- insta/settings.py
+++ insta/settings.py
@@ -31,10 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'photos',
     'bootstrap4',
     'imagekit',
-    'registration',
+    'django_registration',
+    'photos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
